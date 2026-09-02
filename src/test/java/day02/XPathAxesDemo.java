308b190324a1e7494506a3b2c74bc2bf21fbe299
ShreyB <shreypractice.github@gmail.com>
2026-09-02 20:05:11 +0530

    Updates_0209_20:06

---
+++ src/test/java/day02/XPathAxesDemo.java
@@ -0,0 +1,20 @@
+package day02;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class XPathAxesDemo {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://money.rediff.com/gainers/bse/daily/groupa");
+		driver.manage().window().maximize();
+
+	}
+
+}
