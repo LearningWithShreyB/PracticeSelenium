17b0fa5a6285199087a62267ffc6cfbb85e520b4
ShreyB <shreypractice.github@gmail.com>
2026-09-02 17:20:13 +0530

    Updates_0209_17:21

--- src/test/java/day02/CSSLocators.java
+++ src/test/java/day02/CSSLocators.java
@@ -12,9 +12,21 @@ public class CSSLocators {
 		
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://testautomationpractice.blogspot.com/");
+		driver.manage().window().maximize();
 		driver.findElement(By.cssSelector("input#name")).sendKeys("Laptop");
 		Thread.sleep(2000);
 		driver.findElement(By.cssSelector("#name")).sendKeys("Mobile");
+		
+		driver.findElement(By.cssSelector("input.form-check-input")).click();
+		
+		
+		driver.findElement(By.cssSelector("input[placeholder='Enter EMail']")).sendKeys("abc@abc.com");
+		driver.findElement(By.cssSelector("[placeholder='Enter Phone']")).sendKeys("12345678909");
+		
+		
+		driver.findElement(By.cssSelector(".form-check-input[value='female']")).click();
+		
+		driver.quit();
 
 	}
 
