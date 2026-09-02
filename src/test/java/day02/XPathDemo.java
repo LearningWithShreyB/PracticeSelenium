0be7838848625e68cdd46199a915d8840f6031be
ShreyB <shreypractice.github@gmail.com>
2026-09-02 20:03:03 +0530

    Updates_0209_20:03

--- src/test/java/day02/XPathDemo.java
+++ src/test/java/day02/XPathDemo.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class XPathDemo {
@@ -14,9 +15,18 @@ public class XPathDemo {
 		
 		WebDriver driver=new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-		driver.manage().window().maximize();
-		driver.findElement(By.xpath("//*[@placeholder='Username']")).sendKeys("Admin");
+		//driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		//driver.manage().window().maximize();
+		//driver.findElement(By.xpath("//*[@placeholder='Username']")).sendKeys("Admin");
+		
+		driver.get("https://testautomationpractice.blogspot.com/");
+		WebElement male=driver.findElement(By.xpath("//input[@class='form-check-label' and @id='male]"));
+		boolean status1=male.isSelected();
+		System.out.println(status1);
+		male.click();
+		status1=male.isSelected();
+		System.out.println(status1);
+		
 		
 	}
 
